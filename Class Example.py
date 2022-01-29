525023fbeb635116c876ebbb04a3fb5302040cdb
Pythonaire <46537110+Pythonaire@users.noreply.github.com>
2022-01-29 09:41:21 +0100

    Update Class Example.py
    correct description

--- Class Example.py
+++ Class Example.py
@@ -6,57 +6,52 @@ from history import FakeGatoHistory
 
 logging.basicConfig(level=logging.INFO, format="[%(module)s] %(message)s")
 
-NODE_CACHE = {}
-
-def getCache(url, node):
-    requ = {node:"?"} # "?" useless, but needed for dict/json handling
-    ret = {}
-    try:
-        while not node in ret:
-            ret = requests.get(url, json=json.dumps(requ)).json()
-            if node in ret:
-                NODE_CACHE[node] = ret[node]
-                break
-            else:
-                time.sleep(1)
-    except socket.error as e:
-        logging.info('**** request to  {0} timed out: {1}'.format(url, e))
-    return ret
-
-    
-class GardenValues(Accessory):
+class Weather(Accessory):
     category = CATEGORY_SENSOR
-    def __init__(self, node, *args, **kwargs): # Garden sensor nodeNumber 10
+    def __init__(self, node, *args, **kwargs): # Garden sensor nodeNumber 12
         super().__init__(*args, **kwargs)
         self.name = args[1] # args[1] contained the Sensor Name given
         self.node = node # node number of the 433MHz sensor
-        self.set_info_service(firmware_revision='0.0.1', model='Gardener01', manufacturer= 'Pythonaire', serial_number="Soil-001")
-        PlantSensor = self.add_preload_service('PlantSensor', chars=['Name', 'SoilMoisture', 'CurrentTemperature', 'CurrentRelativeHumidity'])
+        self.set_info_service(firmware_revision='0.0.2', model='Gardener01', manufacturer= 'Pythonaire', serial_number="Weather-001")
+        
+        AirTemperature = self.add_preload_service('TemperatureSensor', chars=['Name', 'CurrentTemperature'])
+        AirTemperature.configure_char('Name', value= 'Air Temperature')
+        self.AirTemperature = AirTemperature.configure_char('CurrentTemperature', value= 0) #initial
+
+        AirHumidity = self.add_preload_service('HumiditySensor', chars=['Name', 'CurrentRelativeHumidity'])
+        AirHumidity.configure_char('Name', value= 'Air Humidity')
+        self.AirHumidity = AirHumidity.configure_char('CurrentRelativeHumidity', value = 0) # initial
+
+        AirPressure = self.add_preload_service('AtmosphericPressureSensor', chars=['Name', 'AtmosphericPressure'])
+        AirPressure.configure_char('Name', value= 'Air Pressure')
+        self.AirPressure = AirPressure.configure_char('AtmosphericPressure', value = 0) # initial
+
         Battery = self.add_preload_service("BatteryService", chars=['ChargingState','StatusLowBattery', 'BatteryLevel'])
-        PlantSensor.configure_char('Name', value= 'SoilMoisture')
-        self.SoilMoisture = PlantSensor.configure_char('SoilMoisture', value= 0) #initial
-        self.AirTemperature = PlantSensor.configure_char('CurrentTemperature', value= 0) #initial
-        self.AirHumidity = PlantSensor.configure_char('CurrentRelativeHumidity', value = 0) # initial
         self.BattLevel = Battery.configure_char('BatteryLevel', value = 0)
         self.BattStatus = Battery.configure_char('StatusLowBattery', value = 1)
         Battery.configure_char('ChargingState', value = 2)
 
-        self.url_chached = 'http://rfmgate.home:8001/cached'
-        self.History = FakeGatoHistory('room', self)
+        self.HistoryTerrace = FakeGatoHistory('weather', self)
         
-
-    @Accessory.run_at_interval(3000)
+    @Accessory.run_at_interval(300)
     def run(self):
-        recv = getCache(self.url_chached, str(self.node))
-        logging.info('NodeData :{0}'.format(recv))
-        NodeData = (lambda: {"Charge": 0, "Soil":0, "Hum": 0, "Temp": 0}, lambda: recv[str(self.node)])[recv[str(self.node)] != "None"]()
-        BattStatus = (lambda: 0, lambda: 1)[NodeData["Charge"]<=0]()
+        '''
+        in this example function getCache is used to pull sensor data form a sensor node defined by self.node
+
+        recv = getCache(str(self.node)) # pull sensor date
+        logging.info('NodeData Weather :{0}'.format(recv[str(self.node)]))
+        #{'B':Battery,'AT':AirTemperature,'AH':AirHumidity,'AP':AirPressure,SH':SoilHumidity}
+        NodeData = {"B": 0, "AT":0, "AH": 0, "AP": 0, "SH":0} if recv[str(self.node)] == None else recv[str(self.node)]
+        BattStatus = 1 if NodeData["B"]<=0 else 0
+        '''
         self.BattStatus.set_value(BattStatus)
-        self.SoilMoisture.set_value(NodeData["Soil"])
-        self.AirHumidity.set_value(NodeData["Hum"])
-        self.AirTemperature.set_value(NodeData["Temp"])
-        self.BattLevel.set_value(NodeData["Charge"])
-        self.History.addEntry({'time':int(round(time.time())),'temp':NodeData["Temp"],'humidity': NodeData["Hum"], 'ppm': 0})
+        self.AirHumidity.set_value(NodeData["AH"])
+        self.AirTemperature.set_value(NodeData["AT"])
+        self.AirPressure.set_value(NodeData["AP"])
+        self.BattLevel.set_value(NodeData["B"])
+        self.HistoryTerrace.addEntry({'time':int(round(time.time())),'temp':NodeData["AT"],'humidity': NodeData["AH"], 'pressure':NodeData["AP"]})
+        forward = {'Temp': NodeData["AT"]} 
+    
         
     def stop(self):
         logging.info('Stopping accessory.')
